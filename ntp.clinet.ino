e460466f441a71f42216c907dadb740eb51ded0e
Benjamin Maggi <bmaggi@salesforce.com>
2021-08-06 21:47:12 -0300

    chore: test 74141

--- ntp.clinet.ino
+++ ntp.clinet.ino
@@ -1,6 +1,4 @@
 #include <WiFi.h>
-#include <DHT.h>
-#include <DHT_U.h>
 #include <NTPClient.h>
 // change next line to use with another board/shield
 #include <WiFiUdp.h>
@@ -14,10 +12,23 @@
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
+// Date and time functions using a DS3231 RTC connected via I2C and Wire lib
+#include <RTClib.h>
+RTC_DS3231 rtc;
+
+char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+
+#include <DHT.h>
+#include <DHT_U.h>
+
 #define DHTPIN 25    // modify to the pin we connected
 #define DHTTYPE DHT21   // AM2301 
  
 DHT_Unified dht(DHTPIN, DHTTYPE);
+
+#include <Adafruit_MCP23X17.h>
+#define LED_PIN 0     // MCP23XXX pin LED is attached to
+Adafruit_MCP23X17 mcp;
 
 uint32_t delayMS;
 
@@ -41,6 +52,40 @@ String timeStamp;
 
 void setup(){
   Serial.begin(115200);
+  
+  if (!rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    Serial.flush();
+    abort();
+  }
+
+  if (rtc.lostPower()) {
+    Serial.println("RTC lost power, let's set the time!");
+    // When time needs to be set on a new device, or after a power loss, the
+    // following line sets the RTC to the date & time this sketch was compiled
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    // This line sets the RTC with an explicit date & time, for example to set
+    // January 21, 2014 at 3am you would call:
+    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+  }
+
+  // When time needs to be re-set on a previously configured device, the
+  // following line sets the RTC to the date & time this sketch was compiled
+  // rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  // This line sets the RTC with an explicit date & time, for example to set
+  // January 21, 2014 at 3am you would call:
+  // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+  
+  if (!mcp.begin_I2C()) {
+    Serial.println("MCP initzialization error.");
+    while (1);
+  }
+  // configure pin for output
+  mcp.pinMode(0, OUTPUT);
+  mcp.pinMode(1, OUTPUT);
+  mcp.pinMode(2, OUTPUT);
+  mcp.pinMode(3, OUTPUT);
+  
   WiFi.begin(ssid, password);
 
   if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
@@ -103,6 +148,39 @@ void loop() {
   while(!timeClient.update()) {
     timeClient.forceUpdate();
   }
+  // PORTB = 0
+  mcp.writeGPIO(0b00000000, 1);
+
+  mcp.writeGPIO(0b00000000, 0);
+  delay(500);
+  mcp.writeGPIO(0b00000010, 0);
+  delay(500);
+  mcp.writeGPIO(0b00000011, 0);
+  delay(500);
+  mcp.writeGPIO(0b00000100, 0);
+  delay(500);
+  mcp.writeGPIO(0b00000101, 0);
+  delay(500);
+  mcp.writeGPIO(0b00000110, 0);
+  delay(500);
+  mcp.writeGPIO(0b00000111, 0);
+  delay(500);
+  mcp.writeGPIO(0b00001000, 0);
+  delay(500);
+  mcp.writeGPIO(0b00001001, 0);
+  delay(500);
+  mcp.writeGPIO(0b00001010, 0);
+  delay(500);
+  mcp.writeGPIO(0b00001011, 0);
+  delay(500);
+  mcp.writeGPIO(0b00001100, 0);
+  delay(500);
+  mcp.writeGPIO(0b00001101, 0);
+  delay(500);
+  mcp.writeGPIO(0b00001110, 0);
+  delay(500);
+  mcp.writeGPIO(0b00001111, 0);
+  delay(500);
   // The formattedDate comes with the following format:
   // 2018-05-28T16:00:13Z
   // We need to extract date and time
@@ -161,4 +239,66 @@ void loop() {
     display.display(); 
 
   }
+  printRtcDate();
+}
+
+void printRtcDate() {
+  DateTime now = rtc.now();
+
+  Serial.print(now.year(), DEC);
+  Serial.print('/');
+  Serial.print(now.month(), DEC);
+  Serial.print('/');
+  Serial.print(now.day(), DEC);
+  Serial.print(" (");
+  Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
+  Serial.print(") ");
+  Serial.print(now.hour(), DEC);
+  Serial.print(':');
+  Serial.print(now.minute(), DEC);
+  Serial.print(':');
+  Serial.print(now.second(), DEC);
+  Serial.println();
+
+  Serial.print(" since midnight 1/1/1970 = ");
+  Serial.print(now.unixtime());
+  Serial.print("s = ");
+  Serial.print(now.unixtime() / 86400L);
+  Serial.println("d");
+
+  // calculate a date which is 7 days, 12 hours, 30 minutes, 6 seconds into the future
+  DateTime future (now + TimeSpan(7,12,30,6));
+
+  Serial.print(" now + 7d + 12h + 30m + 6s: ");
+  Serial.print(future.year(), DEC);
+  Serial.print('/');
+  Serial.print(future.month(), DEC);
+  Serial.print('/');
+  Serial.print(future.day(), DEC);
+  Serial.print(' ');
+  Serial.print(future.hour(), DEC);
+  Serial.print(':');
+  Serial.print(future.minute(), DEC);
+  Serial.print(':');
+  Serial.print(future.second(), DEC);
+  Serial.println();
+
+  Serial.print("Temperature: ");
+  Serial.print(rtc.getTemperature());
+  Serial.println(" C");
+
+  Serial.println();
+
+  String formattedDate = timeClient.getFormattedDate(now.unixtime());
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 30);
+  // Display static text
+  display.print(F("Date: "));
+  display.print(formattedDate);
+  display.print("\n");
+  display.display(); 
+    
+  
+  Serial.println(formattedDate);
 }
